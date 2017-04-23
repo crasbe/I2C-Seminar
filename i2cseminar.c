6a69ea02e32b7e3c491312990f2880bf7fc032e2
crasbe <crasbe@gmail.com>
2017-04-23 22:01:37 +0200

    Rohling fuer i2cseminar.c, das Hauptprogramm

---
+++ i2cseminar.c
@@ -0,0 +1,14 @@
+/***********************************************************************
+ * i2cseminar.c
+ * 
+ * Hauptprogramm fuer die Ansteuerung der I2C-Geraete.
+ * 
+ * Autoren: Christopher Buechse und Jan Burmeister
+ * Datum: Sommersemester 2017
+ **********************************************************************/
+
+#include "i2cusb.h"
+
+int main(void) {
+	return 0;
+}
