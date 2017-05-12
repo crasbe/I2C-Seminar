37406cc39be052b3db6fda217d3b554a4a9b16aa
crasbe <crasbe@gmail.com>
2017-05-13 00:48:12 +0200

    Interface für serielle Schnittstelle soweit fertig und kommentiert

--- seriell_linux.c
+++ seriell_linux.c
@@ -1,13 +1,18 @@
-/***********************************************************************
- * seriell_linux.c
+/**
+ * @file seriell_linux.c
  * 
- * Diese Datei enthaelt die Funktionen, die zur Ansteuerung des
- * seriellen Interfaces des USB-ITS-Gerates notwendig sind.
- * Wie der Name impliziert ist diese Datei NUR fuer Linux geeignet!
+ * @brief Linux-Interface für serielle Schnittstelle
  * 
- * Autoren: Christopher Buechse und Jan Burmeister
- * Datum: Sommersemester 2017
- **********************************************************************/
+ * Diese Datei enthält die Funktionen, die zur Ansteuerung des
+ * seriellen Interfaces des USB-ITS-Gerätes notwendig sind.
+ * 
+ * @warning Diese Datei ist NUR für Linux geeignet!
+ * 
+ * @authors Christopher Büchse und Jan Burmeister
+ * @date Sommersemester 2017
+ * 
+ * @see http://man7.org/linux/man-pages/man3/termios.3.html
+ */
 
 #include <errno.h>   // Fehlerausgabe
 #include <fcntl.h>   // Filedeskriptor einstellen
@@ -19,19 +24,29 @@
 
 #include "seriell_linux.h"
 
+/**
+ * @brief Öffnet den seriellen Port
+ * 
+ * Für das serielle Device wird die Termios-Struktur von Linux verwendet.
+ * 
+ * 
+ * @param fd Filedeskriptor des seriellen Devices
+ * @param port Portnummer des zu öffnenden Ports
+ * @return neuer Filedeskriptor 
+ */
 int oeffne_port(int fd, int port) {
 	
 	char portname[] = PORTNAME;
-	portname[sizeof(portname)/sizeof(char)-2] = (char) (port+0x30);
+	portname[sizeof(portname)/sizeof(char)-2] = (char) (port+0x30); // ITOA für Arme...
 	
-	// den seriellen Port oeffnen.
+	// den seriellen Port öffnen.
 	if((fd = open(portname, FLAGS)) < 0) {
 		fprintf(stderr, "Fehler %d beim Oeffnen von %s: %s\n",  errno, portname,
 					  strerror(errno));
 		return -1; // Oeffnen gescheitert
 	}
 	
-	// moegliche andere, gesetzte Flags loeschen.
+	// mögliche andere, gesetzte Flags löschen.
 	fcntl(fd, F_SETFL, 0);
 	
 	// den seriellen Port konfigurieren mit termios
@@ -47,35 +62,52 @@ int oeffne_port(int fd, int port) {
 	cfsetispeed(&seriell, BAUDRATE);
 	cfsetospeed(&seriell, BAUDRATE);
 	
-	// IFlags:
-	//  - IXON, IXOFF, IXANY: Software Flusskontrolle ausschalten
-	//  - IGNBRK: Break-Condition ignorieren
+	/**
+	 * Folgende Flags werden gesetzt:
+	 * 		- C_IFLAG
+	 * 			-# IXON, IXOFF, IXANY (=0):
+	 * 				Software Flusskontrolle ausschalten
+	 * 			-# IGNBRK (=0):
+	 * 				Break-Condition ignorieren
+	 * 		- C_CFLAG
+	 * 			Einstellungen für 8N1
+	 * 			-# PARENB (=0):
+	 * 				Paritykontrolle ausschalten
+	 * 			-# CSTOPB (=0):
+	 * 				Ein Stopbit
+	 * 			-# CSIZE (=CS8):
+	 * 				auf Charaktergröße CS8 setzen
+	 * 			-# CS8:
+	 * 				8 bit pro Zeichen
+	 * 		- C_LFLAG
+	 * 			-# ICANNON (=0):
+	 * 				Mit Rohdaten arbeiten 
+	 * 			-# ECHO (=0):
+	 * 				Empfangene Zeichen nicht wieder zurückgeben
+	 * 			-# ISIG (=0):
+	 * 				Steuersignale (z.B. SIGQUIT) ignorieren
+	 * 		- C_OFLAG
+	 * 			-# OPOST (=0):
+	 * 				Mit Rohdaten arbeiten
+	 * 		- C_CC: Control Characters
+	 * 			-# VMIN (=0):
+	 * 				Es müssen keine Zeichen gesendet werden
+	 * 			-# VTIME (=10):
+	 * 				Timeout auf eine Sekunde setzen
+	 */
+	
 	seriell.c_iflag &= ~(IXON | IXOFF | IXANY);
 	seriell.c_iflag &= IGNBRK;
 	
-	// CFlags: 8N1
-	//  - PARENB: Paritykontrolle ausschalten
-	//  - CSTOPB: ein Stopbit
-	//  - CSIZE: Charactergroesse setzen
-	//  - CS8: 8 bit pro Zeichen
 	seriell.c_cflag &= ~(PARENB | CSTOPB | CSIZE);
 	seriell.c_cflag |= CS8;
 	
-	// LFlags:
-	//  - ICANON: mit Rohdaten arbeiten
-	//  - ECHO: empfangene Zeichen nicht wieder zurueckgeben
-	//  - ISIG: Steuersignale (z.B. SIGQUIT) ignorieren
 	seriell.c_lflag &= ~(ICANON | ECHO | ISIG);
 	
-	// OFlags:
-	//  - OPOST: mit Rohdaten arbeiten
 	seriell.c_oflag &= ~(OPOST);
 	
-	// Control Characters
-	//  - VMIN: minimale Anzahl an zu sendenden Zeichen
-	//  - VTIME: Timeout Zeit
-	seriell.c_cc[VMIN] = 0; // es muessen keine Zeichen gesendet werden
-	seriell.c_cc[VTIME] = 10; // 1s 
+	seriell.c_cc[VMIN] = 0;
+	seriell.c_cc[VTIME] = 10;
 	
 	// Attribute aus der termios Struktur an Filedeskriptor uebergeben
 	//  - TCSANOW: sofort uebernehmen
@@ -91,8 +123,14 @@ int oeffne_port(int fd, int port) {
 	return fd;
 }
 
+/**
+ * @brief Sendet zwei Zeichen über die serielle Schnittstelle
+ * @param fd Filedeskriptor von geöffnetem seriellen Port
+ * @param befehl Zeiger auf zu sendenden Befehl
+ * @return Anzahl gesendeter Zeichen
+ * 
+ */
 int sende_befehl(int fd, char* befehl) {
-	
 	int rueckgabe = write(fd, befehl, 2);
 
 #if DEBUG
@@ -106,6 +144,16 @@ int sende_befehl(int fd, char* befehl) {
 	return rueckgabe;
 }
 
+/**
+ * @brief Liest n Zeichen von der seriellen Schnittstelle
+ * @param fd Filedeskriptor von geöffnetem seriellen Port
+ * @param puffer Puffer für zu lesende Zeichen
+ * @param laenge Anzahl der zu lesenden Bytes
+ * @return Anzahl gelesener Bytes
+ * @note Puffer MUSS mindestens drei Byte groß sein, sollte aber
+ * 			>= laenge sein!
+ * 
+ */
 int lese_antwort(int fd, char* puffer, int laenge) {
 	int gelesene_bytes = read(fd, puffer, laenge);
 	
@@ -121,7 +169,12 @@ int lese_antwort(int fd, char* puffer, int laenge) {
 	return gelesene_bytes;
 }
 
-
+/**
+ * @brief Terminierung des Programms
+ * Diese Funktion schließt den Filedeskriptor und beendet dann das Programm
+ * mit einem Fehlerwert.
+ * @param fd Zu schließender Filedeskriptor
+ */
 void err_quit(int fd) {
 	close(fd);
 	exit(EXIT_FAILURE);
--- seriell_linux.h
+++ seriell_linux.h
@@ -1,38 +1,55 @@
-/***********************************************************************
- * seriell_linux.h
+/**
+ * @file seriell_linux.h
  * 
- * Diese Datei enthaelt alle Definitionen fuer die seriell_linux.c
+ * @brief Linux-Interface für serielle Schnittstelle
+ * 
+ * Diese Datei enthaelt alle Definitionen für die seriell_linux.c
  * Datei.
- * Diese Header-Datei ist NICHT zur direkten Verwendung vorgesehen,
- * sondern kann nur ueber i2cusb.c verwendet werden!
  * 
- * Autoren: Christopher Buechse und Jan Burmeister
- * Datum: Sommersemester 2017
- **********************************************************************/
+ * @warning Diese Header-Datei ist NICHT zur direkten Verwendung vorgesehen,
+ * 			sondern kann nur ueber i2cusb.c verwendet werden!
+ * @warning Diese Datei ist NUR für Linux geeignet!
+ * 
+ * @authors Christopher Büchse und Jan Burmeister
+ * @date Sommersemester 2017
+ */
 
 #ifndef SERIELL_LINUX_H_
 #define SERIELL_LINUX_H_
 
 #include <fcntl.h>   // fuer FD Flags
 #include <termios.h> // fuer Baudrate
+#include <stdio.h>
+#include <unistd.h>
 #include <stdbool.h>
 
-// serieller Port per USB
-#define PORTNAME "/dev/ttyUSBx" // das x wird durch eine Zahl ersetzt
-// die Baudrate als termios Konstante definieren
+/**
+ * @brief Präfix des seriellen Ports
+ * Das 'x' wird später durch die Portnummer ersetzt, ttyUSB ist der
+ * Standard-Port unter Linux für eine serielle Schnittstelle über
+ * USB.
+ */
+#define PORTNAME "/dev/ttyUSBx"
+
+/**
+ * @brief Baudrate des seriellen Ports
+ * @see termios 
+ */
 #define BAUDRATE B38400
 
-/* Flags fuer den seriellen Port:
- *   - O_RDWR: zum Lesen und Schreiben oeffnen
- *   - O_NOCTTY: der serielle Port ist nicht das neue Kontrollterminal
- *              (weiterhin Tastatureingaben annehmen)
- *   - O_SYNC: Schreibzugriffe nicht puffern
+/**
+ * @brief Flags für den seriellen Port
+ * O_RDWR: zum Lesen und Schreiben oeffnen
+ * O_NOCTTY: der serielle Port ist nicht das neue Kontrollterminal (weiterhin Tastatureingaben annehmen)
+ * O_SYNC: Schreibzugriffe nicht puffern
+ * @see termios
  */
 #define FLAGS (O_RDWR | O_NOCTTY | O_SYNC)
 
-
+// Funktionen
 extern int oeffne_port(int port, int baudrate);
 extern int sende_befehl(int fd, char* befehl);
 extern int lese_antwort(int fd, char* puffer, int laenge);
+extern void err_quit(int fd);
 
 #endif // SERIELL_LINUX_H_
